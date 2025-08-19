7ba7a23988aacd0338cfb682a38797ec333684d3
Linvery <23356865+Linvery@users.noreply.github.com>
2025-08-20 01:42:13 +0800

    test1

--- elog.config.js
+++ elog.config.js
@@ -1,13 +1,6 @@
 module.exports = {
   write: {
     platform: 'yuque-pwd',
-    yuque: {
-      token: process.env.YUQUE_TOKEN,
-      login: process.env.YUQUE_LOGIN,
-      repo: process.env.YUQUE_REPO,
-      onlyPublic: false,
-      onlyPublished: true,
-    },
     "yuque-pwd": {
       username: process.env.YUQUE_USERNAME,
       password: process.env.YUQUE_PASSWORD,
@@ -23,21 +16,8 @@ module.exports = {
       token: process.env.HALO_TOKEN,
       policyName: process.env.HALO_POLICY_NAME,
       rawType: 'html',
-      needUploadImage: false,
+      needUploadImage: true,
       formatExt: './customFormatter.js'
-    }
-  },
-  image: {
-    enable: true,
-    platform: 'oss',
-    oss: {
-      secretId: process.env.OSS_SECRET_ID,
-      secretKey: process.env.OSS_SECRET_KEY,
-      bucket: process.env.OSS_BUCKET,
-      region: process.env.OSS_REGION,
-      host: process.env.OSS_HOST,
-      prefixKey: 'elog-docs-images',
-      secretExt: '', // 可选
     }
   }
 }
